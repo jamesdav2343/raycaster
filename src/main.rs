1b905823433d1eccc854f3524b4be12a49d9259b
James <jamesmichaeldavies@outlook.com>
2026-05-03 13:18:26 -0700

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use ggez::conf::{WindowMode, WindowSetup};
-use ggez::{event, ContextBuilder, GameResult};
+use ggez::conf::{ WindowMode, WindowSetup };
+use ggez::{ event, ContextBuilder, GameResult };
 use raycaster::MainState;
 
 const TITLE: &str = "RayCaster";
 
 fn main() -> GameResult {
-    let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
-    let window_setup = WindowSetup::default().title(TITLE).vsync(false);
+    let window_width: f32 = 3024.0;
+    let window_height: f32 = 1964.0;
+
+    let window_mode = WindowMode::default().dimensions(window_width, window_height);
+    let window_setup = WindowSetup::default().title(TITLE).vsync(false).srgb(false);
     let (mut ctx, events_loop) = ContextBuilder::new(TITLE, "migue")
         .window_mode(window_mode)
         .window_setup(window_setup)
